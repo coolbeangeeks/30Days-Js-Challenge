1fe06e8f3d8d5339e3f648f565956a69770b1b09
coolbeangeeks <abhigupta96677@gmail.com>
2024-07-15 17:53:44 +0530

    Day2

---
+++ Day2/activity1.js
@@ -0,0 +1,22 @@
+let t1=5+9
+let t2=5-9
+let t3=5*9
+let t4=9/5;
+let t5=9%5
+
+let t6=1;
+t6+=t1;
+let t7=2;
+t7-=t2;
+
+if(t6>t7)console.log('greater then')
+if(t6<t7)console.log('less then')
+let t8=14;
+let t9='14'
+if(t8===t1){
+    console.log('value and type conversion')
+}
+if(t9==t1){
+    console.log('only value comparsion')
+}
+t2>0 ? console.log('postive'):console.log('negative')
